4aded057921bbdcc1e0cbecd6ee48562a908b4e7
Sachin <thesachinjadhav@gmail.com>
2019-01-07 18:38:26 +0530

    Initial Commit

---
+++ src/components/tourData.js
@@ -0,0 +1,30 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "New York",
+    img: "./img/newyork.jpeg",
+    name: "New York Bridge Tour",
+    info: "Lorem ipsum dolor sit amet consectetur elit. Vel, repellendus!"
+  },
+  {
+    id: 2,
+    city: "Paris",
+    img: "./img/paris.jpeg",
+    name: "Paris Lights Tour",
+    info: "Lorem ipsum dolor sit amet consectetur elit. Vel, repellendus!"
+  },
+  {
+    id: 3,
+    city: "London",
+    img: "./img/london.jpeg",
+    name: "London Royal Palace Tour",
+    info: "Lorem ipsum dolor sit amet consectetur elit. Vel, repellendus!"
+  },
+  {
+    id: 4,
+    city: "Tokyo",
+    img: "./img/tokyo.jpeg",
+    name: "Tokyo Sushi Tour",
+    info: "Lorem ipsum dolor sit amet consectetur elit. Vel, repellendus!"
+  }
+];
